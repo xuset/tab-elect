da6c597664ea68bbbcfa4a5a8db89029a1e8df52
George Farcasiu <farcasiu.george@gmail.com>
2017-01-27 23:27:02 -0600

    Reorganization of class structure and events

--- index.js
+++ index.js
@@ -8,32 +8,99 @@ var EventEmitter = require('events').EventEmitter
 
 TabElect.SUPPORT = IdbKvStore.INDEXEDDB_SUPPORT && IdbKvStore.BROADCAST_SUPPORT
 
+// TODO : expirementally verify that these numbers aren't bullshit
+// Consts
+var LEADER_REFRESH_INTERVAL = 250  // In milliseconds
+var ACK_TIMEOUT = 250
+
+/********************/
+/* PUBLIC INTERFACE */
+/********************/
+
 inherits(TabElect, EventEmitter)
-function TabElect (name, opts) {
+function TabElect (name, opts = {}) {
   var self = this
   if (!TabElect.SUPPORT) throw new Error('No indexDB or BroadcastChannel support')
   if (typeof name === 'undefined') throw new Error('"name" cannot be undefined')
   if (!(self instanceof TabElect)) return new TabElect(name, opts)
-  if (!opts) opts = {}
 
   EventEmitter.call(self)
+
+  self.dbManager = new TabElectDBManager(name)
+  self.dbManager.on('elect', _onElect)
+  self.dbManager.on('depose', _onDepose)
 
   self.isLeader = false
   self.destroyed = false
 
-  self._locking = false
+  // Handle lifecycle events
+  addEventListener('beforeunload', onBeforeUnload)
+
+  function onBeforeUnload () {
+    self._destroy()
+  }
+}
+
+TabElect.prototype.destroy = function () {
+  this._destroy()
+}
+
+TabElect.prototype._onElect = function (cb = noop) {
+  var self = this
+  if (self.destroyed) throw new Error('Already destroyed')
+  if (self.isLeader) throw new Error('Already the leader')
+
+  this.isLeader = true
+  this.emit('active')
+}
+
+TabElect.prototype._onDepose = function () {
+  if (this.destroyed) throw new Error('Already destroyed')
+  if (!this.isLeader) throw new Error('Can not depose when not the leader')
+
+  this.isLeader = false
+  this.emit('deactivate')
+}
+
+TabElect.prototype._destroy = function (err) {
+  if (this.destroyed) return
+  this.destroyed = true
+
+  if (err) this.emit('error', err)
+  this.removeAllListeners()
+
+  self.isLeader = false
+}
+
+/******************/
+/* HELPER CLASSES */
+/******************/
+
+// Manages the database state for the TabElect class
+inherits(TabElectDBManager, EventEmitter)
+function TabElectDBManager (name) {
+  var self = this
+  if (!TabElect.SUPPORT) throw new Error('No indexDB or BroadcastChannel support')
+  if (!(self instanceof TabElectDBManager)) return new TabElectDBManager()
+
+  EventEmitter.call(self)
 
   self._db = new IdbKvStore('tab-elect-' + name)
+  self.destroyed = false
+
+  // Handle DB and lifecycle events
   self._db.on('change', onDbChange)
   self._db.on('error', onDbError)
   self._db.on('close', onDbClose)
 
   addEventListener('beforeunload', onBeforeUnload)
 
-  self.elect()
+  /******************/
+  /* EVENT HANDLERS */
+  /******************/
 
   function onDbChange (change) {
-    self._onDbChange(change)
+    // TODO : handle callbacks for acks and leader elections
   }
 
   function onDbError (err) {
@@ -47,90 +114,39 @@ function TabElect (name, opts) {
   function onBeforeUnload () {
     self._destroy()
   }
+
+  // Find the leader or elect yourself
+  // NOTE that a randomized timeout may be necessary
+  setInterval(function () {
+    self.dbManager.ackLeader(function (succ, err) {
+      // TODO : attempt leader election on err failure
+    })
+  }, LEADER_REFRESH_INTERVAL)
+  self.dbManager.ackLeader()
 }
 
-TabElect.prototype.elect = function (cb) {
-  var self = this
-  cb = cb || noop
-  if (self.destroyed) throw new Error('Already destroyed')
-  if (self.isLeader) throw new Error('Already the leader')
-  if (self._locking) return setTimeout(cb, 0, null, false)
-
-  self._db.remove('lock')
-  .then(function () {
-    return self._lock(cb)
-  })
+// Writes an ack to the DB to be consumed by the listening instance of TabElect
+// cb (succ, err) => succ if leader acks back else err
+TabElectDBManager.prototype.sendAck() {
+  // TODO : write ack to disk and have a failure path through timeout and success path through change notification
 }
 
-TabElect.prototype._lock = function (cb) {
-  var self = this
-  cb = cb || noop
-  if (self.destroyed) return
-  if (self._locking) return cb(null, false)
-
-  self._locking = true
-  return self._db.add('lock', true)
-  .then(function () {
-    self._locking = false
-    self.isLeader = true
-
-    if (self.destroyed) return self._destroyDB()
-
-    cb(null, true)
-    self.emit('elected')
-  })
-  .catch(function (err) {
-    self._locking = false
-
-    if (self.destroyed) return self._destroyDB()
-
-    // ConstraintError - Add operation failed because key already exists. Someone else is leader
-    if (err.name === 'ConstraintError') cb(null, false)
-    else cb(err)
-  })
+TabElectDBManager.prototype.elect() {
+  // TODO : correctly handle attempting to win an election
 }
 
-TabElect.prototype.depose = function () {
-  if (this.destroyed) throw new Error('Already destroyed')
-  if (!this.isLeader) throw new Error('Can not depose when not the leader')
-
-  this._db.remove('lock')
-  this._onDepose()
-}
-
-TabElect.prototype._onDepose = function () {
-  this.isLeader = false
-  this.emit('deposed')
-}
-
-TabElect.prototype._onDbChange = function (change) {
-  if (this.destroyed || change.key !== 'lock' || change.method !== 'remove') return
-
-  if (this.isLeader) {
-    // Someone removed our lock so we are not the leader anymore
-    this._onDepose()
-  } else {
-    // The leaders lock has been removed so attempt to elect ourselves
-    this._lock()
-  }
-}
-
-TabElect.prototype.destroy = function () {
-  this._destroy()
-}
-
-TabElect.prototype._destroy = function (err) {
+TabElectDBManager.prototype._destroy(err) {
   if (this.destroyed) return
   this.destroyed = true
 
   if (err) this.emit('error', err)
   this.removeAllListeners()
 
-  this._destroyDB()
-  self.isLeader = false
+  // TODO : clean up the DB connection
 }
 
-TabElect.prototype._destroyDB = function () {
+// TODO : refactor this into the _destroy method
+TabElectDBManager.prototype._destroyDB = function () {
   var self = this
   if (!self._db) return
 
@@ -142,6 +158,10 @@ TabElect.prototype._destroyDB = function () {
     self._db = null
   }
 }
+
+/********************/
+/* HELPER FUNCTIONS */
+/********************/
 
 function noop () {
   // do nothing
